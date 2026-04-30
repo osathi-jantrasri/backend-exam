0bb72a42964a31e3ec452fb12a46ce303bbd5a32
Osathi Jantrasri <otfor01@gmail.com>
2026-04-30 13:18:37 +0700

    1.4 finish

--- 1_logic_test/4_number_to_roman/main.py
+++ 1_logic_test/4_number_to_roman/main.py
@@ -19,5 +19,28 @@ output = number can not less than 0
 
 class Solution:
 
+    roman_map = [
+        (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
+        (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),
+        (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+    ]
+
     def number_to_roman(self, number: int) -> str:
-        pass
+        if number < 0:
+            return "number can not less than 0"
+        if number == 0:
+            return "nulla"
+
+        result = ""
+        for value, symbol in self.roman_map:
+            count, number = divmod(number, value)
+            result += symbol * count
+        return result
+
+# if __name__ == "__main__":
+#     sol = Solution()
+#     print(sol.number_to_roman(1001))     
+#     print(sol.number_to_roman(-1))  
+#     print(sol.number_to_roman(0))
+#     print(sol.number_to_roman(653))
+#     print(sol.number_to_roman(3999))
